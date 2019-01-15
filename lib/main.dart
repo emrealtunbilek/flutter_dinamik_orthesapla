1ea87ddc4f7462a78ac24a32269c8eff7afcc6fb
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-16 02:40:48 +0300

    değerlerin girileceği formun hazırlanması

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   String dersAdi;
+  int dersKredi = 1;
+  double dersHarfDegeri = 4;
 
   @override
   Widget build(BuildContext context) {
@@ -46,27 +47,79 @@ class _MyHomePageState extends State<MyHomePage> {
           //STATIC FORMU TUTAN CONTAINER
           Expanded(
               child: Container(
-                padding: EdgeInsets.all(10),
-            color: Colors.pink,
-            child: Form(child: Column(
+            padding: EdgeInsets.all(10),
+            color: Colors.pink.shade200,
+            child: Form(
+                child: Column(
               children: <Widget>[
-
                 TextFormField(
                   decoration: InputDecoration(
                     labelText: "Ders Adı",
                     hintText: "Ders adını giriniz",
-                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(fontSize: 18),
+                    labelStyle: TextStyle(fontSize: 22),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple, width: 2),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.purple, width: 2),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
                   ),
-                  validator: (girilenDeger){
-                    if(girilenDeger.length>0){
+                  validator: (girilenDeger) {
+                    if (girilenDeger.length > 0) {
                       return null;
-                    }else return "Ders adı boş olamaz";
+                    } else
+                      return "Ders adı boş olamaz";
                   },
-                  onSaved: (kaydedilecekDeger){
+                  onSaved: (kaydedilecekDeger) {
                     dersAdi = kaydedilecekDeger;
                   },
-                )
-
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      child: DropdownButton<int>(
+                        items: dersKredileriItems(),
+                        value: dersKredi,
+                        onChanged: (secilenKredi) {
+                          setState(() {
+                            dersKredi = secilenKredi;
+                          });
+                        },
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                      margin: EdgeInsets.only(top: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                    ),
+                    Container(
+                      child: DropdownButton<double>(
+                        items: dersHarfDegerleriItems(),
+                        value: dersHarfDegeri,
+                        onChanged: (secilenHarf) {
+                          setState(() {
+                            dersHarfDegeri = secilenHarf;
+                          });
+                        },
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+                      margin: EdgeInsets.only(top: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                    ),
+                  ],
+                ),
+                Divider(color: Colors.blue, height: 40, indent: 2,),
               ],
             )),
           )),
@@ -74,11 +127,91 @@ class _MyHomePageState extends State<MyHomePage> {
           //DINAMIK LISTE TUTAN CONTAINER
           Expanded(
               child: Container(
-            color: Colors.green,
+            color: Colors.green.shade200,
             child: Text("Liste"),
           )),
         ],
       ),
     );
+  }
+
+  List<DropdownMenuItem<int>> dersKredileriItems() {
+    List<DropdownMenuItem<int>> krediler = [];
+
+    for (int i = 1; i <= 10; i++) {
+//     var aa = DropdownMenuItem<int>(value: i, child: Text("$i Kredi"),)
+//     krediler.add(aa);
+      krediler.add(DropdownMenuItem<int>(
+        value: i,
+        child: Text(
+          "$i Kredi",
+          style: TextStyle(fontSize: 30),
+        ),
+      ));
+    }
+
+    return krediler;
+  }
+
+  List<DropdownMenuItem<double>> dersHarfDegerleriItems() {
+    List<DropdownMenuItem<double>> harfler = [];
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " AA ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 4,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " BA ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 3.5,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " BB ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 3,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " CB ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 2.5,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " CC ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 2,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " DC ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 1.5,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " DD ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 1,
+    ));
+    harfler.add(DropdownMenuItem(
+      child: Text(
+        " FF ",
+        style: TextStyle(fontSize: 30),
+      ),
+      value: 0,
+    ));
+
+    return harfler;
   }
 }
